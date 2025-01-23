7f2cbc62a06ede9307db689b33781ca8527c4f93
mferreyra <mferreyra82@gmail.com>
2025-01-23 20:28:33 -0300

    changed function names to OpenApi naming convention for CRUD

--- main.py
+++ main.py
@@ -15,15 +15,12 @@ async def root():
     return {"message": "Hola Mundo! desde FASTAPI"}
 
 
-@app.get("/post")
-def get_post():
+@app.get("/posts")
+def get_posts():
     return {"data" : "post data"}
 
 
-@app.post("/create_post")
-async def create_post(post: Post):
-    #print(post)
-    #return {"t": post.title, "c": post.content, "p": post.is_public, "r": post.rating}
-    return post.model_dump()
-    #return { "data" : post.model_dump()}
-    #return post.model_dump_json()
+@app.post("/posts")
+async def create_posts(post: Post):
+    return { "data" : post.model_dump()}
+    
