5af99fc4ed827f69ad99ec42135221e11aa89dc6
mferreyra <mferreyra82@gmail.com>
2025-01-23 19:26:50 -0300

    adding pydantic schema

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+from typing import Optional
 from fastapi import Body, FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Post(BaseModel):
+    title: str
+    content: str
+    is_public: bool = True #* este parámetro asi definido por defecto es verdadero
+    rating: Optional[int] = None #* este parámetro asi definido es opcional y por defecto es nulo (from typing import Optional)
 
 @app.get("/")
 async def root():
@@ -14,6 +21,9 @@ def get_post():
 
 
 @app.post("/create_post")
-async def create_post(payload: dict = Body(...)):
-    print(payload)
-    return {"message": payload["title"]}
+async def create_post(post: Post):
+    #print(post)
+    #return {"t": post.title, "c": post.content, "p": post.is_public, "r": post.rating}
+    return post.model_dump()
+    #return { "data" : post.model_dump()}
+    #return post.model_dump_json()
