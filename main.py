e10b450c99483c12048bd26e80cc0fbff233bd18
matias-x555 <mferreyra82@gmail.com>
2025-01-23 15:07:53 -0300

    add post example

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
-from fastapi import FastAPI
+from fastapi import Body, FastAPI
 
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hola Mundo - FASTAPI"}
+    return {"message": "Hola Mundo! desde FASTAPI"}
+
+
+@app.get("/post")
+def get_post():
+    return {"data" : "post data"}
+
+
+@app.post("/create_post")
+def create_post(payload: dict = Body(...)):
+    print(payload)
+    return {"message": "post created!"}
